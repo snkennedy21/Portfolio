c55d8ec397e53f6466ffe07c246073ed92bee2d0
Sean Kennedy <snkennedy21@gmail.com>
2022-04-13 11:18:09 -0400

    add hero-title slide animation

--- script.js
+++ script.js
@@ -2,33 +2,57 @@
 
 window.addEventListener('load', function () {
   const heroTitleLetters = document.querySelectorAll('.hero-title-letter');
+  const socialNavigation = document.querySelector('.social-navigation');
+  const siteNavigation = document.querySelector('.site-navigation');
+  const heroTitleRole = document.querySelector('.hero-title__role');
 
   heroTitleLetters.forEach((letter, i) => {
     setTimeout(() => {
       letter.classList.remove('hero-title__name__letter-hidden');
     }, i * 200);
   })
+
+  setTimeout(() => {
+    socialNavigation.classList.remove('invisible');
+    socialNavigation.style.animation = 'zoomFadeIn 1s ease'
+
+    siteNavigation.classList.remove('invisible');
+    siteNavigation.style.animation = 'zoomFadeIn 1s ease'
+
+  }, 3000);
+
+  setTimeout(() => {
+    heroTitleRole.classList.remove('invisible');
+    heroTitleRole.classList.remove('translate-left');
+    heroTitleRole.style.animation = 'slideInBounce 1s ease'
+  }, 2300);
 })
 
 
+// This function adds/removes shake animation to social-navigation icons through event delegation on mouseover/mouseout
+const socialNavigationIconAnimation = function () {
+  const socialNavigation = document.querySelector('.social-navigation__list');
 
-const socialNavigation = document.querySelector('.social-navigation__list');
+  // Add shake animation
+  socialNavigation.addEventListener('mouseover', function (e) {
+    if (e.target.classList.contains('social-navigation__icon')) {
+      const icon = e.target;
+      icon.style.animation = 'shake 0.5s ease';
+    } else if (e.target.classList.contains('social-navigation__icon__name')) {
+      const icon = e.target.previousElementSibling;
+      icon.style.animation = 'shake 0.5s ease';
+    }
+  });
 
-socialNavigation.addEventListener('mouseover', function (e) {
-  if (e.target.classList.contains('social-navigation__icon')) {
-    const icon = e.target;
-    icon.style.animation = 'shake 0.5s ease';
-  } else if (e.target.classList.contains('social-navigation__icon__name')) {
-    const icon = e.target.previousElementSibling;
-    icon.style.animation = 'shake 0.5s ease';
-  }
-});
-socialNavigation.addEventListener('mouseout', function (e) {
-  if (e.target.classList.contains('social-navigation__icon')) {
-    const icon = e.target;
-    icon.style.animation = '';
-  } else if (e.target.classList.contains('social-navigation__icon__name')) {
-    const icon = e.target.previousElementSibling;
-    icon.style.animation = '';
-  }
-});
+  // Remove shake animation
+  socialNavigation.addEventListener('mouseout', function (e) {
+    if (e.target.classList.contains('social-navigation__icon')) {
+      const icon = e.target;
+      icon.style.animation = '';
+    } else if (e.target.classList.contains('social-navigation__icon__name')) {
+      const icon = e.target.previousElementSibling;
+      icon.style.animation = '';
+    }
+  });
+}
+socialNavigationIconAnimation();
